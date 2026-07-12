38b3b684095552474b53c9206c1f630f2a77496c
Your GitHub Username <fadel_h669@hotmail.com>
2026-07-12 11:13:58 +0300

    Frontend - added Task service

---
+++ frontend/lib/services/task_service.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+
+import '../core/constants/api_constants.dart';
+import '../models/task.dart';
+
+class TaskService {
+  final Dio _dio =Dio(
+    BaseOptions(
+      baseUrl: ApiConstants.baseUrl,
+    )
+  );
+
+
+  Future<List<Task>> getTasks(String token) async {
+    final response = await _dio.get(
+      ApiConstants.tasks,
+      options: Options(
+        headers: {
+           "Authorization": "Bearer $token",
+        }
+      )
+    );
+
+    return (response.data as List)
+    .map((task) =>Task.fromJson(task))
+    .toList();
+  }
+}
